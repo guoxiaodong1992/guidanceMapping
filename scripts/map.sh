161ac11e7e6185304aa456f090ade2fd4211f448
guoxiaodong1992 <guoxiaodong@sjtu.edu.cn>
2017-11-16 05:11:49 +0800

    Version With Kinect Mapping, NO Odom

--- scripts/map.sh
+++ scripts/map.sh
@@ -6,7 +6,7 @@ PATH=/bin:/sbin:/usr/bin/:/usr/sbin/:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 source ~/catkin_ws/devel/setup.bash
 rosparam set use_sim_time true
-rosbag play  --clock ~/catkin_ws/sdata/2017-10-19-12-58-05.bag > verylog.txt
+rosbag play  --clock ~/catkin_ws/sdata/2017-11-13-16-18-17.bag > verylog.txt
 
 
 
